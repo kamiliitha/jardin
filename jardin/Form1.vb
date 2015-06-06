10f80e60f867d6f953c4ee8c9a2a408f8bc333c1
kamiliitha <azocar_89@hotmail.com>
2015-06-06 13:08:39 -0300

    cambios2

--- jardin/Form1.vb
+++ jardin/Form1.vb
@@ -1,3 +1,37 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+Public Class Form1
 
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Using cnn As New SqlConnection("Data Source=(local);Initial Catalog=jardin;Integrated Security=True")
+            cnn.Open()
+
+            Dim consulta As String = "Select password From usuario where nombre=@Nombre"
+            Dim cmd As New SqlCommand(consulta, cnn)
+            Dim nombre As String = "admin"
+            Dim clave As String = "admin"
+            cmd.Parameters.AddWithValue("@Nombre", usuario.Text)
+            If usuario.Text = "" Or pass.Text = "" Then
+                MsgBox("Faltan datos necesarios", MsgBoxStyle.Critical, "Error Login")
+            Else
+                If usuario.Text = nombre And pass.Text = clave Then
+                    Me.Visible = False
+                    plataforma_administrador.Visible = True
+                Else
+                    Dim Password As String = cmd.ExecuteScalar()
+                    If Password = pass.Text Then
+                        Me.Visible = False
+                        plataforma_usuario.Visible = True
+                    Else
+                        MsgBox("Error de credenciales", MsgBoxStyle.Critical, "Error Login")
+                    End If
+                End If
+
+            End If
+        End Using
+        'comentarios 
+    End Sub
 End Class
---
+++ jardin/ingresar usuario.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ingresar_usuario
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ingresar_usuario))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.nombre = New System.Windows.Forms.TextBox()
+        Me.password = New System.Windows.Forms.TextBox()
+        Me.password2 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "NOMBRE USUARIO"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(81, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "CONTRASEÑA"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(131, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "REPETIR CONTRASEÑA"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(77, 198)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "GUARDAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(210, 198)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "CERRAR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'nombre
+        '
+        Me.nombre.Location = New System.Drawing.Point(185, 42)
+        Me.nombre.Name = "nombre"
+        Me.nombre.Size = New System.Drawing.Size(100, 20)
+        Me.nombre.TabIndex = 5
+        '
+        'password
+        '
+        Me.password.Location = New System.Drawing.Point(185, 89)
+        Me.password.Name = "password"
+        Me.password.Size = New System.Drawing.Size(100, 20)
+        Me.password.TabIndex = 6
+        '
+        'password2
+        '
+        Me.password2.Location = New System.Drawing.Point(185, 134)
+        Me.password2.Name = "password2"
+        Me.password2.Size = New System.Drawing.Size(100, 20)
+        Me.password2.TabIndex = 7
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(311, 116)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(172, 105)
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'ingresar_usuario
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Violet
+        Me.ClientSize = New System.Drawing.Size(485, 249)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.password2)
+        Me.Controls.Add(Me.password)
+        Me.Controls.Add(Me.nombre)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "ingresar_usuario"
+        Me.Text = "ingresar_usuario"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents nombre As System.Windows.Forms.TextBox
+    Friend WithEvents password As System.Windows.Forms.TextBox
+    Friend WithEvents password2 As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+End Class
