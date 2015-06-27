a4a5dbab96b22d316ff14af66b4884c455a0577a
kamiliitha <azocar_89@hotmail.com>
2015-06-27 00:10:30 -0300

    cambios de plataforma usuario

---
+++ jardin/plataforma usuario.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class plataforma_usuario
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(plataforma_usuario))
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ALUMNOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.INGRESARToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MODIFICARToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ELIMINARToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DOCENTEToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.INGRSARToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MODIFICARToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ELIMINARToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CURSOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.INGRESARToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MODIFICARToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ELIMINARToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CUPOCURSOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.INGRESARToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MODIFICARToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ELIMINARToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.COMIDAToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.INGRESARToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MODIFICARToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ELIMINARToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.INVENTARIOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.INGRESARToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MODIFICARToolStripMenuItem5 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ELIMINARToolStripMenuItem5 = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(316, 143)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 13)
+        Me.Label8.TabIndex = 41
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(-122, 120)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 13)
+        Me.Label2.TabIndex = 35
+        Me.Label2.Text = "DOCENTE"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(-119, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 33
+        Me.Label1.Text = " ALUMNO"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(43, 220)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(172, 103)
+        Me.PictureBox1.TabIndex = 56
+        Me.PictureBox1.TabStop = False
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(241, 286)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 53
+        Me.Button8.Text = "CERRAR"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(353, 152)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 50
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ALUMNOToolStripMenuItem, Me.DOCENTEToolStripMenuItem, Me.CURSOToolStripMenuItem, Me.CUPOCURSOToolStripMenuItem, Me.COMIDAToolStripMenuItem, Me.INVENTARIOToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(625, 24)
+        Me.MenuStrip1.TabIndex = 69
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ALUMNOToolStripMenuItem
+        '
+        Me.ALUMNOToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.INGRESARToolStripMenuItem, Me.MODIFICARToolStripMenuItem, Me.ELIMINARToolStripMenuItem})
+        Me.ALUMNOToolStripMenuItem.Name = "ALUMNOToolStripMenuItem"
+        Me.ALUMNOToolStripMenuItem.Size = New System.Drawing.Size(70, 20)
+        Me.ALUMNOToolStripMenuItem.Text = "ALUMNO"
+        '
+        'INGRESARToolStripMenuItem
+        '
+        Me.INGRESARToolStripMenuItem.Name = "INGRESARToolStripMenuItem"
+        Me.INGRESARToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.INGRESARToolStripMenuItem.Text = "INGRESAR"
+        '
+        'MODIFICARToolStripMenuItem
+        '
+        Me.MODIFICARToolStripMenuItem.Name = "MODIFICARToolStripMenuItem"
+        Me.MODIFICARToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.MODIFICARToolStripMenuItem.Text = "MODIFICAR"
+        '
+        'ELIMINARToolStripMenuItem
+        '
+        Me.ELIMINARToolStripMenuItem.Name = "ELIMINARToolStripMenuItem"
+        Me.ELIMINARToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ELIMINARToolStripMenuItem.Text = "ELIMINAR"
+        '
+        'DOCENTEToolStripMenuItem
+        '
+        Me.DOCENTEToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.INGRSARToolStripMenuItem, Me.MODIFICARToolStripMenuItem1, Me.ELIMINARToolStripMenuItem1})
+        Me.DOCENTEToolStripMenuItem.Name = "DOCENTEToolStripMenuItem"
+        Me.DOCENTEToolStripMenuItem.Size = New System.Drawing.Size(72, 20)
+        Me.DOCENTEToolStripMenuItem.Text = "DOCENTE"
+        '
+        'INGRSARToolStripMenuItem
+        '
+        Me.INGRSARToolStripMenuItem.Name = "INGRSARToolStripMenuItem"
+        Me.INGRSARToolStripMenuItem.Size = New System.Drawing.Size(137, 22)
+        Me.INGRSARToolStripMenuItem.Text = "INGRESAR"
+        '
+        'MODIFICARToolStripMenuItem1
+        '
+        Me.MODIFICARToolStripMenuItem1.Name = "MODIFICARToolStripMenuItem1"
+        Me.MODIFICARToolStripMenuItem1.Size = New System.Drawing.Size(137, 22)
+        Me.MODIFICARToolStripMenuItem1.Text = "MODIFICAR"
+        '
+        'ELIMINARToolStripMenuItem1
+        '
+        Me.ELIMINARToolStripMenuItem1.Name = "ELIMINARToolStripMenuItem1"
+        Me.ELIMINARToolStripMenuItem1.Size = New System.Drawing.Size(137, 22)
+        Me.ELIMINARToolStripMenuItem1.Text = "ELIMINAR"
+        '
+        'CURSOToolStripMenuItem
+        '
+        Me.CURSOToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.INGRESARToolStripMenuItem1, Me.MODIFICARToolStripMenuItem2, Me.ELIMINARToolStripMenuItem2})
+        Me.CURSOToolStripMenuItem.Name = "CURSOToolStripMenuItem"
+        Me.CURSOToolStripMenuItem.Size = New System.Drawing.Size(57, 20)
+        Me.CURSOToolStripMenuItem.Text = "CURSO"
+        '
+        'INGRESARToolStripMenuItem1
+        '
+        Me.INGRESARToolStripMenuItem1.Name = "INGRESARToolStripMenuItem1"
+        Me.INGRESARToolStripMenuItem1.Size = New System.Drawing.Size(137, 22)
+        Me.INGRESARToolStripMenuItem1.Text = "INGRESAR"
+        '
+        'MODIFICARToolStripMenuItem2
+        '
+        Me.MODIFICARToolStripMenuItem2.Name = "MODIFICARToolStripMenuItem2"
+        Me.MODIFICARToolStripMenuItem2.Size = New System.Drawing.Size(137, 22)
+        Me.MODIFICARToolStripMenuItem2.Text = "MODIFICAR"
+        '
+        'ELIMINARToolStripMenuItem2
+        '
+        Me.ELIMINARToolStripMenuItem2.Name = "ELIMINARToolStripMenuItem2"
+        Me.ELIMINARToolStripMenuItem2.Size = New System.Drawing.Size(137, 22)
+        Me.ELIMINARToolStripMenuItem2.Text = "ELIMINAR"
+        '
+        'CUPOCURSOToolStripMenuItem
+        '
+        Me.CUPOCURSOToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.INGRESARToolStripMenuItem2, Me.MODIFICARToolStripMenuItem3, Me.ELIMINARToolStripMenuItem3})
+        Me.CUPOCURSOToolStripMenuItem.Name = "CUPOCURSOToolStripMenuItem"
+        Me.CUPOCURSOToolStripMenuItem.Size = New System.Drawing.Size(92, 20)
+        Me.CUPOCURSOToolStripMenuItem.Text = "CUPO CURSO"
+        '
+        'INGRESARToolStripMenuItem2
+        '
+        Me.INGRESARToolStripMenuItem2.Name = "INGRESARToolStripMenuItem2"
+        Me.INGRESARToolStripMenuItem2.Size = New System.Drawing.Size(137, 22)
+        Me.INGRESARToolStripMenuItem2.Text = "INGRESAR"
+        '
+        'MODIFICARToolStripMenuItem3
+        '
+        Me.MODIFICARToolStripMenuItem3.Name = "MODIFICARToolStripMenuItem3"
+        Me.MODIFICARToolStripMenuItem3.Size = New System.Drawing.Size(137, 22)
+        Me.MODIFICARToolStripMenuItem3.Text = "MODIFICAR"
+        '
+        'ELIMINARToolStripMenuItem3
+        '
+        Me.ELIMINARToolStripMenuItem3.Name = "ELIMINARToolStripMenuItem3"
+        Me.ELIMINARToolStripMenuItem3.Size = New System.Drawing.Size(137, 22)
+        Me.ELIMINARToolStripMenuItem3.Text = "ELIMINAR"
+        '
+        'COMIDAToolStripMenuItem
+        '
+        Me.COMIDAToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.INGRESARToolStripMenuItem3, Me.MODIFICARToolStripMenuItem4, Me.ELIMINARToolStripMenuItem4})
+        Me.COMIDAToolStripMenuItem.Name = "COMIDAToolStripMenuItem"
+        Me.COMIDAToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.COMIDAToolStripMenuItem.Text = "COMIDA"
+        '
+        'INGRESARToolStripMenuItem3
+        '
+        Me.INGRESARToolStripMenuItem3.Name = "INGRESARToolStripMenuItem3"
+        Me.INGRESARToolStripMenuItem3.Size = New System.Drawing.Size(137, 22)
+        Me.INGRESARToolStripMenuItem3.Text = "INGRESAR"
+        '
+        'MODIFICARToolStripMenuItem4
+        '
+        Me.MODIFICARToolStripMenuItem4.Name = "MODIFICARToolStripMenuItem4"
+        Me.MODIFICARToolStripMenuItem4.Size = New System.Drawing.Size(137, 22)
+        Me.MODIFICARToolStripMenuItem4.Text = "MODIFICAR"
+        '
+        'ELIMINARToolStripMenuItem4
+        '
+        Me.ELIMINARToolStripMenuItem4.Name = "ELIMINARToolStripMenuItem4"
+        Me.ELIMINARToolStripMenuItem4.Size = New System.Drawing.Size(137, 22)
+        Me.ELIMINARToolStripMenuItem4.Text = "ELIMINAR"
+        '
+        'INVENTARIOToolStripMenuItem
+        '
+        Me.INVENTARIOToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.INGRESARToolStripMenuItem4, Me.MODIFICARToolStripMenuItem5, Me.ELIMINARToolStripMenuItem5})
+        Me.INVENTARIOToolStripMenuItem.Name = "INVENTARIOToolStripMenuItem"
+        Me.INVENTARIOToolStripMenuItem.Size = New System.Drawing.Size(87, 20)
+        Me.INVENTARIOToolStripMenuItem.Text = "INVENTARIO"
+        '
+        'INGRESARToolStripMenuItem4
+        '
+        Me.INGRESARToolStripMenuItem4.Name = "INGRESARToolStripMenuItem4"
+        Me.INGRESARToolStripMenuItem4.Size = New System.Drawing.Size(137, 22)
+        Me.INGRESARToolStripMenuItem4.Text = "INGRESAR"
+        '
+        'MODIFICARToolStripMenuItem5
+        '
+        Me.MODIFICARToolStripMenuItem5.Name = "MODIFICARToolStripMenuItem5"
+        Me.MODIFICARToolStripMenuItem5.Size = New System.Drawing.Size(137, 22)
+        Me.MODIFICARToolStripMenuItem5.Text = "MODIFICAR"
+        '
+        'ELIMINARToolStripMenuItem5
+        '
+        Me.ELIMINARToolStripMenuItem5.Name = "ELIMINARToolStripMenuItem5"
+        Me.ELIMINARToolStripMenuItem5.Size = New System.Drawing.Size(137, 22)
+        Me.ELIMINARToolStripMenuItem5.Text = "ELIMINAR"
+        '
+        'plataforma_usuario
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Violet
+        Me.ClientSize = New System.Drawing.Size(625, 335)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "plataforma_usuario"
+        Me.Text = "plataforma_usuario"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ALUMNOToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents INGRESARToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MODIFICARToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ELIMINARToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DOCENTEToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents INGRSARToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MODIFICARToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ELIMINARToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CURSOToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents INGRESARToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MODIFICARToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ELIMINARToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CUPOCURSOToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents INGRESARToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MODIFICARToolStripMenuItem3 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ELIMINARToolStripMenuItem3 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents COMIDAToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents INGRESARToolStripMenuItem3 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MODIFICARToolStripMenuItem4 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ELIMINARToolStripMenuItem4 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents INVENTARIOToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents INGRESARToolStripMenuItem4 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MODIFICARToolStripMenuItem5 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ELIMINARToolStripMenuItem5 As System.Windows.Forms.ToolStripMenuItem
+End Class
